5fd2c8f97471b2fd111073198519ed64660ebe6e
Thibault MINNEBOO <thibault.minneboo@etudiant.univ-reims.fr>
2022-11-05 03:16:14 +0100

    Add event handler

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import AtayaClient from "./structure/AtayaClient";
 import CommandHandler from "./structure/CommandHandler";
+import EventHandler from "./structure/EventHandler";
 
 const client = new AtayaClient(process.env.TOKEN_APP);
 const commandHandler = new CommandHandler(__dirname + '/commands/', client);
+const eventHandler = new EventHandler(__dirname + '/events/', client);
 
 client.initialize();
+eventHandler.run();
 commandHandler.run();
